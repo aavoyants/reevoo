dbb6353e40c4fdad73bf9e64b0322044ab699826
aavoyants <aavoyants@gmail.com>
2012-12-26 14:22:49 +0200

    reevoo

--- app/models/checkout.rb
+++ app/models/checkout.rb
@@ -2,23 +2,23 @@ class Checkout
   attr_accessor :total, :rules, :products
 
   def initialize(pricing_rules = [])
-    @products = [] 
+    @products = []
     @total = 0.00
     @pricing_rules = pricing_rules
   end
-   
+
   def scan(product)
     @products << product.dup
   end
-   
-  def total    
+
+  def total
     if !@products.empty?
       if !@pricing_rules.empty?
-	@pricing_rules.each { |rule| rule.apply_discount(@products) }
+        @pricing_rules.each { |rule| rule.apply_discount(@products) }
       end
       @products.each {|product| @total += product.price}
-    end 
-    
-    return @total
+    end
+
+    @total
   end
 end
--- app/models/rule.rb
+++ app/models/rule.rb
@@ -1,36 +1,36 @@
 class Rule < ActiveRecord::Base
   attr_accessible :id, :name
   validates :name, :presence => true
-   
+
   def apply_discount(products)
     if respond_to? name
-      return send(name, products)
-    end    
+      send(name, products)
+    end
   end
-  
+
   def buy_one_get_one_free(products)
     counter = 0
-    products.each do |product|   
+    products.each do |product|
       if (product.rule_id == id)
-	counter += 1
-	if (counter % 2 == 0)
-	  product.price = 0
-	end
+        counter += 1
+          if (counter % 2 == 0)
+            product.price = 0
+          end
+        end
+      end
+
+    products
+  end
+
+  def bulk_purchase(products)
+    products.each do |product|
+      if (product.rule_id == id)
+        if (products.count {|p| p.code == product.code}) >= 3
+          product.price = product.price * 0.9
+        end
       end
     end
 
-    return products
-  end
-  
-  def bulk_purchase(products)
-    products.each do |product|    
-      if (product.rule_id == id)
-	if (products.count {|p| p.code == product.code}) >= 3
-	  product.price = product.price * 0.9
-	end
-      end
-    end
-    
-    return products
+    products
   end
 end
